ec9037e918cdb6e76c8806f270158574b90ee153
Anton-Koniouchevsky <anton.koniouchevsky@gmail.com>
2018-02-20 10:15:13 +0300

    Add quadratic-equation implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let coeffs = equation.replace(/\s/g, '').match(/[^\^]\d+/g);
+    let a = coeffs[0],
+        b = coeffs[1],
+        c = coeffs[2];
+
+    let discRoot = Math.sqrt(b * b - 4 * a * c);
+
+    return [(-b - discRoot) / (2 * a),
+        (-b + discRoot) / (2 * a)
+    ].map(x => Math.round(x)).sort((left, right) => left - right);
 }
